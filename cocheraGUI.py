869c6a571880a677cebf0d8d8f874ab3c3aeb2ac
yandivd <yandivd@gmail.com>
2021-08-31 16:48:55 -0700

    comentado de lo que deberia hacer el label

--- cocheraGUI.py
+++ cocheraGUI.py
@@ -30,6 +30,7 @@ botonClean.grid(row=5, column=0, pady=5)
 botonExit=Button(miFrame, text="Salir",width=12, height=2)
 botonExit.grid(row=6, column=0, pady=5)
 
+#Label que deberia mostrar la informacion de los autos almacenados
 labelListado=Label(miFrame, height=12, width=30)
 labelListado.grid(row=2, column=1,rowspan=4)
 labelListado.config(background="white")
